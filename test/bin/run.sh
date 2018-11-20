3b067b005d309de318b21dacfa0bc1f9019b7bfc
Dragos Petrescu <dragos.petrescu@stud.acs.upb.ro>
2018-11-20 15:37:41 +0200

    removed time command

--- test/bin/run.sh
+++ test/bin/run.sh
@@ -9,7 +9,7 @@ FOLDER=$1
 cd $FOLDER
 make compile
 
-qsub -sync y -e ../test/output/out1.e -o ../test/output/out1.o -q ibm-dp.q time ./blur ../test/input/in1.ppm ../test/output/out1.ppm 15 3
+qsub -sync y -e ../test/output/out1.e -o ../test/output/out1.o -q ibm-dp.q ./blur ../test/input/in1.ppm ../test/output/out1.ppm 15 3
 
 diff ../test/output/out1.ppm ../test/ref/ref1.ppm
 if [ $? -eq 0 ]; then
