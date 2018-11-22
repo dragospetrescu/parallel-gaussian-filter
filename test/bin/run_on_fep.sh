892fc3784eb6749fd6c1ee871148363ca82427e1
Dragos Petrescu <dragos.petrescu@stud.acs.upb.ro>
2018-11-22 02:18:31 +0200

    wip2

--- test/bin/run_on_fep.sh
+++ test/bin/run_on_fep.sh
@@ -9,7 +9,7 @@ FOLDER=$1
 cd $FOLDER
 make compile
 
-qsub -cwd -sync y -e ~/gaussian-filter/test/output/out1.e -o ~/gaussian-filter/test/output/out1.o -q ibm-dp.q ~/gaussian-filter/test/bin/run.sh $FOLDER
+qsub -cwd -sync y -e ~/gaussian-filter/test/output/out1.e -o ~/gaussian-filter/test/output/out1.o -q ibm-dp.q ~/gaussian-filter/test/bin/run.sh 
 
 diff ../test/output/out1.ppm ../test/ref/ref1.ppm
 if [ $? -eq 0 ]; then
--- test/send_command_to_fep.sh
+++ test/send_command_to_fep.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 
-ssh fep ~/gaussian-filter/test/bin/run_on_fep.sh serial
+ssh fep gaussian-filter/test/bin/run_on_fep.sh serial
 
