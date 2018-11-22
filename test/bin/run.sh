1037eabbbc1a7a40bbce29218ca8100f38e61e2a
dragos.petrescu <dragos.petrescu@fep7-1.grid.pub.ro>
2018-11-22 03:07:20 +0200

    fixed testing scripts

--- test/bin/run.sh
+++ test/bin/run.sh
@@ -1,3 +1,4 @@
 #!/bin/bash
 
-~/gaussian-filter/serial/blur ~/gaussian-filter/test/input/in1.ppm ~/gaussian-filter/test/output/out1.ppm 15 3
+cd
+./gaussian-filter/$1/blur gaussian-filter/test/input/in1.ppm gaussian-filter/test/output/out1.ppm 15 3
--- test/bin/run_on_fep.sh
+++ test/bin/run_on_fep.sh
@@ -9,12 +9,18 @@ FOLDER=$1
 cd $FOLDER
 make compile
 
-qsub -cwd -sync y -e ~/gaussian-filter/test/output/out1.e -o ~/gaussian-filter/test/output/out1.o -q ibm-dp.q ~/gaussian-filter/test/bin/run.sh 
+mkdir ~/gaussian-filter/test/output/ &> /dev/null
+qsub -sync y -o ~/gaussian-filter/test/output/out1.o -e ~/gaussian-filter/test/output/out1.e -q ibm-dp.q ~/gaussian-filter/test/bin/run.sh $FOLDER
 
 diff ../test/output/out1.ppm ../test/ref/ref1.ppm
 if [ $? -eq 0 ]; then
+	echo "OUTPUTS ARE IDENTICAL!"
 	cat ../test/output/out1.e
 	cat ../test/output/out1.o
 else
 	echo "THE OUTPUT IS DIFFERENT FROM THE REFFERENCE!"
 fi
+
+mv ~/gaussian-filter/test/output/out1.o ~/gaussian-filter/test/output/out1.o_bkp &> /dev/null
+mv ~/gaussian-filter/test/output/out1.e ~/gaussian-filter/test/output/out1.e_bkp &> /dev/null
+
