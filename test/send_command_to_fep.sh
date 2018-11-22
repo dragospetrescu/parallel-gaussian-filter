d169fb96384e90f7320f95ac0a490b98fcc3fee0
Dragos Petrescu <dragos.petrescu@stud.acs.upb.ro>
2018-11-22 03:15:10 +0200

    last fixes to remote testing scripts

--- test/send_command_to_fep.sh
+++ test/send_command_to_fep.sh
@@ -1,4 +1,10 @@
 #!/bin/bash
 
-ssh fep gaussian-filter/test/bin/run_on_fep.sh serial
+if [ -z "$1" ]
+then
+	echo "NU AI SPECIFICAT FOLDERUL! Ex: serial, io, etc"
+	exit 1
+fi
+
+ssh fep gaussian-filter/test/bin/run_on_fep.sh $1
 
