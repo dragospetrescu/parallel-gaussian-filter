a9065e912b378204f6e09819d4e764961391813f
Dragos Petrescu <dragos.petrescu@stud.acs.upb.ro>
2018-12-04 01:23:31 +0200

    high sample rate

--- test/bin/run.sh
+++ test/bin/run.sh
@@ -3,4 +3,5 @@
 export OMP_NUM_THREADS=$2
 module load compilers/solarisstudio-12.5
 cd
-collect ./gaussian-filter/$1/blur gaussian-filter/test/input/in1.ppm gaussian-filter/test/output/out1.ppm 15 3
+
+collect -p hi ./gaussian-filter/$1/blur gaussian-filter/test/input/in1.ppm gaussian-filter/test/output/out1.ppm 15 3
