eb063343b7ea17edc67c4c51e5da9cbe385dd35c
Dragos Petrescu <dragos.petrescu@stud.acs.upb.ro>
2018-12-04 01:52:36 +0200

    added NUM_THREADS for pthreads

--- test/bin/run.sh
+++ test/bin/run.sh
@@ -1,7 +1,12 @@
 #!/bin/bash
 
-export OMP_NUM_THREADS=$2
+
 module load compilers/solarisstudio-12.5
 cd
+if [ $1 = "openmp+io" ]; then
+   export OMP_NUM_THREADS=$2
+else
+    sed -i "s/#define NUM_THREADS [0-9.]\+/#define NUM_THREADS ${2}/"  gaussian-filter/pthreads/src/filter.h
+fi
 
 collect -p hi ./gaussian-filter/$1/blur gaussian-filter/test/input/in1.ppm gaussian-filter/test/output/out1.ppm 15 3
