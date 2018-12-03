f03e377d0743be816ce9c4a1e937a8230eb33d48
Dragos Petrescu <dragos.petrescu@stud.acs.upb.ro>
2018-12-04 01:16:55 +0200

    added collect

--- test/bin/run.sh
+++ test/bin/run.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
 
 export OMP_NUM_THREADS=$2
+module load compilers/solarisstudio-12.5
 cd
-./gaussian-filter/$1/blur gaussian-filter/test/input/in1.ppm gaussian-filter/test/output/out1.ppm 15 3
+collect ./gaussian-filter/$1/blur gaussian-filter/test/input/in1.ppm gaussian-filter/test/output/out1.ppm 15 3
