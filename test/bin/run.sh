4e22ef5abfd69b3bd1e28652031472f58405cafc
Dragos Petrescu <dragos.petrescu@stud.acs.upb.ro>
2018-12-04 02:08:45 +0200

    make clean beacause .h are not in makefile

--- test/bin/run.sh
+++ test/bin/run.sh
@@ -7,7 +7,9 @@ if [ $1 = "openmp+io" ]; then
    export OMP_NUM_THREADS=$2
 else
     sed -i "s/#define NUM_THREADS [0-9.]\+/#define NUM_THREADS ${2}/" gaussian-filter/pthreads/src/filter.h
+    make -C gaussian-filter/pthreads clean
     make -C gaussian-filter/pthreads
+
 fi
 
 collect -p hi ./gaussian-filter/$1/blur gaussian-filter/test/input/in1.ppm gaussian-filter/test/output/out1.ppm 15 3
