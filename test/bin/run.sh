fe2e26a219090bcfde89cbc3ce3a95eb9b433ad5
Dragos Petrescu <dragos.petrescu@stud.acs.upb.ro>
2018-12-11 14:40:40 +0200

    added mpi

--- test/bin/run.sh
+++ test/bin/run.sh
@@ -5,11 +5,16 @@ module load compilers/solarisstudio-12.5
 cd
 if [ $1 = "openmp+io" ]; then
    export OMP_NUM_THREADS=$2
-else
+else if [ $1 = "pthreads" ]; then
     sed -i "s/#define NUM_THREADS [0-9.]\+/#define NUM_THREADS ${2}/" gaussian-filter/pthreads/src/filter.h
     make -C gaussian-filter/pthreads clean
     make -C gaussian-filter/pthreads
 
 fi
 
-collect -p hi ./gaussian-filter/$1/blur gaussian-filter/test/input/in2.ppm gaussian-filter/test/output/out2.ppm 15 3
+
+if [ $1 = "mpi" ]; then
+    collect -p hi mpirun -np $2 ./gaussian-filter/$1/blur gaussian-filter/test/input/in2.ppm gaussian-filter/test/output/out2.ppm 15 3
+else
+    collect -p hi ./gaussian-filter/$1/blur gaussian-filter/test/input/in2.ppm gaussian-filter/test/output/out2.ppm 15 3
+fi
